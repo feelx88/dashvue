b82e76e0a9d993eb1ac3ce6b461f9c59be8262e5
Felix Müller <feelx88@googlemail.com>
2018-02-04 14:51:21 +0100

    Made switch widget extend from general widget.

---
+++ src/App/widgets/Widget.vue
@@ -0,0 +1,40 @@
+<template>
+  <c-flex :size="size">
+      <v-card>
+        <v-card-title v-if="props.title">
+            {{ props.title }}
+        </v-card-title>
+        <slot></slot>
+      </v-card>
+  </c-flex>
+</template>
+
+<script lang="ts">
+
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({
+    props: {
+        props: Object,
+        size: {
+            type: String,
+            default: '1'
+        }
+    }
+})
+export default class Widget extends Vue {
+
+    defaults(): any {
+        return {};
+    }
+
+    created (): void {
+        const defaults = this.defaults();
+        for (const key of Object.keys(defaults)) {
+            this.$props.props[key] = this.$props.props[key] || defaults[key];
+        }
+    }
+}
+
+</script>
--- src/App/widgets/switch/Switch.vue
+++ src/App/widgets/switch/Switch.vue
@@ -1,40 +1,24 @@
 <template>
-  <c-flex :size="size">
-        <v-card>
-            <v-card-title primary-title>
-                {{ props.title }}
-            </v-card-title>
-            <v-card-text>
-                <v-switch :label="value ? props.textOn : props.textOff" v-model="value"></v-switch>
-            </v-card-text>
-        </v-card>
-    </c-flex>
+  <widget :props="props" :size="size">
+    <v-card-text>
+        <v-switch :label="value ? props.textOn : props.textOff" v-model="value"></v-switch>
+    </v-card-text>
+  </widget>
 </template>
 
 <script lang="ts">
 
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import Widget from '../Widget.vue';
 
-@Component({
-    props: {
-        props: Object,
-        size: {
-            type: String,
-            default: '1'
-        }
-    }
-})
-export default class Switch extends Vue {
+@Component
+export default class Switch extends Widget {
     value: boolean = false;
-    defaults: any = {
-        textOn: 'On',
-        textOff: 'Off'
-    }
-
-    created (): void {
-        for (const key of Object.keys(this.defaults)) {
-            this.$props.props[key] = this.$props.props[key] || this.defaults[key];
+    defaults(): any {
+        return {
+            textOn: 'On',
+            textOff: 'Off'
         }
     }
 }
