a7135d753df069a0c4df16bbfc75ad940f085b3b
Felix Müller <feelx88@googlemail.com>
2018-02-04 14:51:32 +0100

    Added simple text widget.

---
+++ src/App/widgets/text/Text.vue
@@ -0,0 +1,19 @@
+<template>
+    <widget :props="props" :size="size">
+        <v-card-text>
+            {{ props.text }}
+        </v-card-text>
+    </widget>
+</template>
+
+<script lang="ts">
+
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import Widget from '../Widget.vue';
+
+@Component
+export default class Text extends Widget {
+}
+
+</script>
--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ import App from './App/App.vue';
 
 import Switch from "./App/widgets/switch/Switch.vue";
 Vue.component('widget-switch', Switch);
+import Text from "./App/widgets/text/Text.vue";
+Vue.component('widget-text', Text);
 
 new Vue({
   el: '#app',
