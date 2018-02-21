8d1506fc0aa0db287bc3167cb4e81231465b8d47
Felix Müller <feelx88@googlemail.com>
2018-02-21 21:48:31 +0100

    Made page types configurable and added current default type as page-widget component.

---
+++ frontend/src/App/pages/WidgetPage/WidgetPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container grid-list-md>
+        <v-layout row wrap v-for="(row, rowIndex) in props.widgets" :key="rowIndex">
+            <component v-for="(widget, widgetIndex) in props.widgets[rowIndex]" :key="`${props.title}_${rowIndex}_${widgetIndex}`" :is="`widget-${widget.type}`" :props="widget.props" :size="widget.size">
+            </component>
+        </v-layout>
+    </v-container>
+</template>
+
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import Page from "../Page.vue";
+
+@Component
+export default class WidgetPage extends Page {}
+</script>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,34 +1,39 @@
-import '../node_modules/vuetify/src/stylus/app.styl';
+import "../node_modules/vuetify/src/stylus/app.styl";
 
-import Vue from 'vue';
-import Vuetify from 'vuetify';
+import Vue from "vue";
+import Vuetify from "vuetify";
 Vue.use(Vuetify);
 
-import VueCookie from 'vue-cookie';
+import VueCookie from "vue-cookie";
 Vue.use(VueCookie);
 
 import ConfigurableFlex from "./App/ConfigurableFlex.vue";
-Vue.component('c-flex', ConfigurableFlex);
-import Login from './App/Login.vue';
-Vue.component('login', Login);
+Vue.component("c-flex", ConfigurableFlex);
+import Login from "./App/Login.vue";
+Vue.component("login", Login);
 
-import App from './App/App.vue';
+import App from "./App/App.vue";
 
-import HttpOutput from './App/datasources/outputs/Http.vue'
-Vue.component('output-http', HttpOutput);
-import HttpInput from './App/datasources/inputs/Http.vue'
-Vue.component('input-http', HttpInput);
+import HttpOutput from "./App/datasources/outputs/Http.vue";
+Vue.component("output-http", HttpOutput);
+import HttpInput from "./App/datasources/inputs/Http.vue";
+Vue.component("input-http", HttpInput);
 
-import Widget from './App/widgets/Widget.vue';
-Vue.component('widget', Widget);
+import Page from "./App/pages/Page.vue";
+Vue.component("page", Page);
+import WidgetPage from "./App/pages/WidgetPage/WidgetPage.vue";
+Vue.component("page-widgets", WidgetPage);
+
+import Widget from "./App/widgets/Widget.vue";
+Vue.component("widget", Widget);
 import Switch from "./App/widgets/switch/Switch.vue";
-Vue.component('widget-switch', Switch);
+Vue.component("widget-switch", Switch);
 import Text from "./App/widgets/text/Text.vue";
-Vue.component('widget-text', Text);
+Vue.component("widget-text", Text);
 import MediaController from "./App/widgets/media-controller/MediaController.vue";
-Vue.component('widget-media-controller', MediaController);
+Vue.component("widget-media-controller", MediaController);
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
 });
